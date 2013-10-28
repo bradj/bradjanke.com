cdb0306b55f7f52cc9af1680d3108fcc0c48ec08
Brad Janke <brad.janke@gmail.com>
2013-10-27 22:57:12 -0500

    Traded tabs for spaces.

--- nado.py
+++ nado.py
@@ -3,11 +3,11 @@ import tornado.web
 from views import index
 
 class MainHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.write(index.view())
+    def get(self):
+        self.write(index.view())
 
 application = tornado.web.Application([ (r'/', MainHandler), ])
 
 if __name__ == "__main__":
-	application.listen(8888)
-	tornado.ioloop.IOLoop.instance().start()
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
